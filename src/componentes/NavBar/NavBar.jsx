2ac47ecd1a19e554279452a88dbfcfec16d29237
chico20200 <andreschico@live.com>
2025-01-04 17:33:08 -0500

    carpetas de componentes

---
+++ src/componentes/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+export const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <ul>
+                    <li><a href="">INICIO</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">ETIQUETAS</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">ETIQUETAS PERSONALIZADAS</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">DONDE COMPRAR</a></li>
+                </ul>
+                <ul>
+                    <li><a href="">COMENTARIOS</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
