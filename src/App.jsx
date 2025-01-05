d2e697105910ee453f53ab9f12ef87012e57c765
chico20200 <andreschico@live.com>
2025-01-04 19:33:37 -0500

    parte de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { useState } from 'react'
 import './App.css'
 import NavBar from './componentes/NavBar/NavBar'
+import Inicio from './componentes/Inicio/Inicio'
 function App() {
   return (
     <>
       <div className = "App">
         <NavBar/>
+        <Inicio/>
       </div>
     </>
   )
---
+++ src/componentes/Inicio/Inicio.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Inicio-styles.css"
+function Inicio() {
+  return (
+    <section className='container'>
+            <div className='fondo'>
+                <h1>AMERICAN LABEL</h1>
+                <p>
+                “Somos pioneros en innovación de maquinaria para ofrecer productos de alta calidad a nuestros clientes.”
+                </p>
+                <button>MAS INFORMACIÓN</button>
+            </div>
+    </section>
+  )
+}
+
+export default Inicio
